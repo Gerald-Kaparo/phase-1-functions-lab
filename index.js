386668b934dad82126c3beb252cdc5a479ac2663
Gerald <kapgerado05@gmail.com>
2022-09-27 10:15:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (newDist) {
+    if (newDist<42){
+      return(42-newDist);
+    }
+    else {
+  
+    return(newDist-42);
+  }
+  }
+  
+  
+  
+  function distanceFromHqInFeet (newDist) {
+    return distanceFromHqInBlocks(newDist)*264;
+  }
+  
+  function distanceTravelledInFeet(numF,numL){
+    if (numL>numF){
+      return((numL-numF)*264);
+    }
+    else
+      return((numF-numL)*264);
+  }
+  
+  
+  function calculatesFarePrice(pointSt,pointStp){
+    let jarny = distanceTravelledInFeet(pointSt,pointStp);
+    if (jarny<= 400){
+      return 0;
+    }
+    if (jarny>=400 && jarny<=2000){
+      return (jarny-400)* 0.02;
+    }
+    else if(jarny>=2000&& jarny<=2500){
+      return 25;
+    }
+    else if (jarny > 2500){
+      return "cannot travel that far";
+    }
+  }
